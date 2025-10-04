48d14338d191c54a4e9022f1f8bf2147e198faa8
Chx999 <jinjiechen25@gmail.com>
2025-10-04 22:27:48 +0200

    Hecho,pero super raro

--- clase_problema/problemas/Nombre_ambicios.py
+++ clase_problema/problemas/Nombre_ambicios.py
@@ -1,13 +1,11 @@
 def perfecte(nre: int) -> bool:
-    divisors_nre = []  # Genera la lista de divisores de nre
-    for divisor in range(1, nre):
-        if nre % divisor == 0:
-            divisors_nre.append(divisor)
-    print(divisors_nre)
+    #    divisors_nre = []  # Genera la lista de divisores de nre(sin incluir el mismo nre)
+    #    for divisor in range(1, nre):
+    #        if nre % divisor == 0:
+    #            divisors_nre.append(divisor)
+    divisors_nre = [i for i in range(1, nre) if nre % i == 0]
 
-    suma_divisors = 0  # Calcula la suma de los divisores
-    for suma in divisors_nre:
-        suma_divisors = suma_divisors + suma
+    suma_divisors = sum(divisors_nre)  # Calcula la suma de los divisores
 
     if suma_divisors == nre:  # Comprueva si la suma de divosores es igual a nre
         return True
@@ -16,7 +14,15 @@ def perfecte(nre: int) -> bool:
 
 
 def ambicios(num: int) -> bool:
-    pass
+    number_got = [int(1)]
+    while num not in number_got:
+        number_got.append(num)
+        divisor = [i for i in range(1, num) if num % i == 0]
+        print(f"Divisores propios de {num} es {divisor}")
+        num = sum(divisor)
+    print(f"Durante el proceso los numeros conseguidos son {number_got[1:]}")
+    return True if perfecte(number_got[-1]) else False
 
 
-print(perfecte(25))
+# print(perfecte(100))
+print(ambicios(143))
